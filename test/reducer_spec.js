4f4bf3118c6c72cb558d8d7629aedbaa01dd5dbd
dhammar <ddh@iastate.edu>
2016-07-19 20:03:54 -0500

    added reducer function and some tests

---
+++ test/reducer_spec.js
@@ -0,0 +1,63 @@
+import {Map, fromJS} from 'immutable';
+import {expect} from 'chai';
+
+import reducer from '../src/reducer';
+
+describe('reducer', () => {
+
+	it('handles SET_ENTRIES', () => {
+		const initialState = Map();
+		const action = {type : 'SET_ENTRIES', entries : ['Resevoir Dogs']};
+		const nextState = reducer(initialState, action);
+
+		expect(nextState).to.equal(fromJS({
+			entries : ['Resevoir Dogs']
+		}));
+	});
+
+	it('handles NEXT', () => {
+		const initialState = fromJS({
+			entries : ['Resevoir Dogs', 'Jackie Brown', 'Hateful Eight']
+		});
+		const action = {type : 'NEXT'};
+		const nextState = reducer(initialState, action);
+
+		expect(nextState).to.equal(fromJS({
+			vote : {
+				pair : ['Resevoir Dogs', 'Jackie Brown']
+			},
+			entries : ['Hateful Eight']
+		}));
+	});
+
+	it('handles VOTE', () => {
+		const initialState = fromJS({
+                vote: {
+                    pair : ['Resevoir Dogs', 'Jackie Brown']
+                },
+                entries : []
+            });
+
+		const action = {type : 'VOTE', entry : 'Jackie Brown'};
+		const nextState = reducer(initialState, action);
+
+		expect(nextState).to.equal(fromJS({
+                vote: {
+                    pair : ['Resevoir Dogs', 'Jackie Brown'],
+                    tally : {'Jackie Brown' : 1}
+                },
+                entries : []
+                
+        }));
+	});
+
+	it('has an initial state', () => {
+		const action = {type : 'SET_ENTRIES', entries : ['Resevoir Dogs']};
+		const nextState = reducer(undefined, action);
+		expect(nextState).to.equal(fromJS({
+			entries : ['Resevoir Dogs']
+		}));
+	});
+
+
+});

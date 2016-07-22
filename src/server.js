49e7f663fab8d4200b0ac197af71944277b436d1
dhammar <ddh@iastate.edu>
2016-07-22 15:43:30 -0500

    small fixes but still broken

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 import Server from 'socket.io'
 
-export default function startServer(store) {
+
+export function startServer(store) {
 	const io = new Server().attach(9427);
 
 	store.subscribe( () => 
 		io.emit('state', store.getState().toJS())
 	);
 
-	io.on('connection' (socket) => {
+	io.on('connection', (socket) => {
 		socket.emit('state', store.getState.toJS());
 	});
 }

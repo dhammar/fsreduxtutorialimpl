37ca34863932b139b031f485fff81372fafaf6a9
dhammar <ddh@iastate.edu>
2016-07-26 07:50:26 -0500

    added action handling and little json dataset

---
+++ entries/entries.js
@@ -0,0 +1,10 @@
+[
+	"Kill Bill: Vol. 1",
+	"Kill Bill: Vol. 2",
+	"The Hateful Eight",
+	"Inglorious Basterds",
+	"Resevoir Dogs",
+	"Django Unchained",
+	"Jackie Brown",
+	"Pulp Fiction"
+]
--- src/server.js
+++ src/server.js
@@ -10,5 +10,6 @@ export function startServer(store) {
 
 	io.on('connection', (socket) => {
 		socket.emit('state', store.getState.toJS());
+		socket.on('action', store.dispatch.bind(store));
 	});
 }

135fb97fda3afa581be367c1ad143377c8801e8a
dhammar <ddh@iastate.edu>
2016-08-01 16:37:58 -0500

    added routing, but versioning of packages incorrect

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route, Router, hashHistory} from 'react-router';
+import App from './components/App'
 import Voting from './components/Voting';
+import Results from './components/Results';
 
 const pair = ['Resevoir Dogs', 'Kill Bill Vol. 2'];
 
+const routes = <Route component={App}>
+	<Route path="/results" component={Results}/>
+	<Route path="/" component={Voting} />
+</Route>;
+
 ReactDOM.render(
-	<Voting pair={pair} winner="Resevoir Dogs" />,
+	<Router history={hashHistory}>{routes}</Router>,
 	document.getElementById('app')
-)
+);
